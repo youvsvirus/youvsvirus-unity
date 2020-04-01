0794d849ef9833507214fb8ae6a22988eba85873
Klitz, Margrit <margrit.klitz@dlr.de>
2020-04-01 09:41:41 +0200

    Start Menue - rest

---
+++ YouVsVirus/Assets/Scripts/UI/TextChange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class TextChange : MonoBehaviour
+{
+    private TMP_Text m_TextComponent;
+     
+    // Change the text
+    public void ChangeText(float f)
+    {
+        // Get a reference to the text component.
+        m_TextComponent = GetComponent<TMP_Text>();
+        // the number in the text is set by the slider
+        m_TextComponent.text = "Social Distancing: " + f.ToString() + '%';
+    }
+}
