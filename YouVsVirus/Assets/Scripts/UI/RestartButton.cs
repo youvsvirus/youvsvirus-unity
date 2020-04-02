1d34dd92d091b9c015ba48811aa46d81b3a7f39d
Holke <Johannes.Holke@dlr.de>
2020-04-02 16:31:09 +0200

    Added a Restart and a Quit Button to the end screen.
    We can now choose to restart the game or to quit it.

---
+++ YouVsVirus/Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Load first level
+/// </summary>
+public class RestartButton : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        // Restart Button has been pressed, we go back to the main menu
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
