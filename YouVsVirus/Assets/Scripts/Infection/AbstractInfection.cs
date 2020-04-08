5ef32afe5c1eeea20ea356a547733d89fb5728a5
Frederik Hennig <fredy.hng@gmail.com>
2020-04-08 14:03:32 +0200

    AbstractInfection and bug fix.

---
+++ YouVsVirus/Assets/Scripts/Infection/AbstractInfection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Components;
+
+namespace Infection
+{
+    /// <summary>
+    /// Base class for different infection models.
+    /// Attach to a human to control its infection behaviour.
+    /// </summary>
+    [RequireComponent(typeof(HumanBase))]
+    public abstract class AbstractInfection : MonoBehaviour
+    {
+        /// <summary>
+        /// My Human. Is set once in Start() and can be accessed by derived classes.
+        /// </summary>
+        protected HumanBase myHuman { get; private set; }
+        
+        public virtual void Start()
+        {
+            myHuman = GetComponent<HumanBase>();
+        }
+
+        public abstract void Update();
+
+        /// <summary>
+        /// Notify the infection that the human has been exposed.
+        /// </summary>
+        public abstract void Expose();
+
+        /// <summary>
+        /// Implemented in a derived class, checks and updates my human's 
+        /// condition depending on the infection model.
+        /// </summary>
+        public abstract void UpdateCondition();
+    }
+}
---
+++ YouVsVirus/Assets/Scripts/Infection/ProbabilityBasedInfection.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Components;
+
+namespace Infection
+{
+    public class ProbabilityBasedInfection : AbstractInfection
+    {
+
+        /// <summary>
+        /// How long my human has been exposed to the virus
+        /// </summary>
+        private int daysSinceExposure = 0;
+
+        /// <summary>
+        /// Contains parmams about how fast infection spreads
+        /// </summary>
+        private InfectionControl infectionControl;
+
+        /// <summary>
+        /// Initialize this infection.
+        /// </summary>
+        public override void Start()
+        {
+            base.Start();
+            //  Find the infection control instance
+            GameObject InfectionControl = GameObject.Find("InfectionControl");
+            infectionControl = InfectionControl.GetComponent<InfectionControl>();
+        }
+
+        public override void Update()
+        {
+            if (infectionControl.IsNewDay())
+            {
+                UpdateCondition();
+            }
+        }
+
+        /// <summary>
+        /// Notify the infection that the human has been exposed.
+        /// </summary>
+        public override void Expose()
+        {
+            daysSinceExposure = 0;
+        }
+
+        /// <summary>
+        /// Update the human's condition
+        /// </summary>
+        public override void UpdateCondition()
+        {
+
+            switch (myHuman.GetCondition())
+            {
+                case HumanBase.EXPOSED:
+                    {
+                        // Damn, we're EXPOSED. But wINFECTIOUS the sickness actually break out?
+                        //  Incubation time has passed without infection --> recovered!
+                        if (daysSinceExposure > infectionControl.IncubationTime)
+                        {
+                            myHuman.SetCondition(HumanBase.RECOVERED);
+                            return;
+                        }
+
+                        //  Maybe it breaks out today?
+                        if (Random.value <= infectionControl.OutbreakRate)
+                        {
+                            myHuman.SetCondition(HumanBase.INFECTIOUS);
+                            return;
+                        }
+
+                        daysSinceExposure++;
+                        return;
+                    }
+
+                case HumanBase.INFECTIOUS:
+                    {
+                        // Maybe we recover today...
+                        if (Random.value <= infectionControl.RecoveryRate)
+                        {
+                            myHuman.SetCondition(HumanBase.RECOVERED);
+                            return;
+                        }
+
+                        // Maybe we die today.
+                        if (Random.value <= infectionControl.DeathRate)
+                        {
+                            myHuman.SetCondition(HumanBase.DEAD);
+                            return;
+                        }
+
+                        return;
+                    }
+
+                default: return;
+            }
+        }
+    }
+}
