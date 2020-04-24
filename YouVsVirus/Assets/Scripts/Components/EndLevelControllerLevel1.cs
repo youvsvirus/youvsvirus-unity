ce5655bf22e9b2cdd2d995ce5fe96279a7888a42
Johannes Holke <johannes.holke@dlr.de>
2020-04-25 01:55:43 +0200

    This is a huge commit (sorry). Added a second level.
    We add a second level.
    Currently, this level is the same as the first one, but with
    a supermarket on the map.
    The supermarket currently does nothing.
    Added a new end screen after the first level that prints
    stats and lets the user continue to the second leveÃl.

--- YouVsVirus/Assets/Scripts/Components/EndLevelControllerLevel1.cs
+++ YouVsVirus/Assets/Scripts/Components/EndLevelControllerLevel1.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 
 /// <summary>
 /// The end level controller for level 1.
-/// Level 1 just ends when everybody was infected, so
-/// we keep all settings from the base class.
+/// Level 1 just ends everybody was infected
+/// and when it ends call level 2.
 /// </summary>
 public class EndLevelControllerLevel1 : EndLevelControllerBase
 {
-
+    /// <summary>
+    /// Triggers the end of the level.
+    /// Level1 calls levels
+    /// </summary>
+    public override void EndLevel()
+    {
+        // Load the End Scene of the game
+        UnityEngine.SceneManagement.SceneManager.LoadScene("EndScreenLevel1");
+    }
 }
---
+++ YouVsVirus/Assets/Scripts/Components/EndLevelControllerLevel2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+/// <summary>
+/// The end level controller for level 1.
+/// Level 2 ends when everybody was infected and
+/// when finished calls the end screen so
+/// we keep all settings from the base class.
+/// </summary>
+public class EndLevelControllerLevel2 : EndLevelControllerBase
+{
+
+}
