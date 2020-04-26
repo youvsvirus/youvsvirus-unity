de89010735c8a1bdfe48761bb8195b2676339985
Klitz, Margrit <margrit.klitz@dlr.de>
2020-04-26 07:18:21 +0200

    Added end level controller, no condition implemented yet

---
+++ YouVsVirus/Assets/Scripts/Components/EndLevelControllerLevel3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+/// <summary>
+/// The end level controller for level 3.
+/// Level 3 ends negative when either player or friend
+/// get infected and ends positive when both reach the
+/// exit sign.
+/// </summary>
+public class EndLevelControllerLevel3 : EndLevelControllerBase
+{
+
+}
