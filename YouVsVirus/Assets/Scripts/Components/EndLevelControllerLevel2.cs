f12da205ca0bf4929dce727a9fcb707aa8bb94b6
Klitz, Margrit <margrit.klitz@dlr.de>
2020-04-26 14:46:10 +0200

    after level 2 ends the screen to continue to level 3 is loaded

--- YouVsVirus/Assets/Scripts/Components/EndLevelControllerLevel2.cs
+++ YouVsVirus/Assets/Scripts/Components/EndLevelControllerLevel2.cs
@@ -5,12 +5,19 @@ using System.Diagnostics;
 using UnityEngine;
 
 /// <summary>
-/// The end level controller for level 1.
+/// The end level controller for level 2.
 /// Level 2 ends when everybody was infected and
 /// when finished calls the end screen so
 /// we keep all settings from the base class.
 /// </summary>
 public class EndLevelControllerLevel2 : EndLevelControllerBase
 {
-
+    /// <summary>
+    /// Triggers the end of the level.
+    /// </summary>
+    public override void EndLevel()
+    {
+        // Load end screen
+        UnityEngine.SceneManagement.SceneManager.LoadScene("EndScreenLevel2");
+    }
 }
