39500777d57f48c37c0cdd26cc17ed9bfa26a7ea
Klitz, Margrit <margrit.klitz@dlr.de>
2020-05-02 14:01:22 +0200

    Make NPCs spawn randomly on the dance floor

--- YouVsVirus/Assets/Scripts/leveldisco/CreatePopLeveldisco.cs
+++ YouVsVirus/Assets/Scripts/leveldisco/CreatePopLeveldisco.cs
@@ -98,11 +98,26 @@ namespace Components
             // give the friend a unique id
             Player.myID = numNPCs+1;
 
-            // place 100 NPCs on dancefloor (contained by cirlce edge collider)
+            // get the dancefloor that we want to place the npcs on
+            CircleEdgeCollider2D dancefloor = GetComponentInChildren<CircleEdgeCollider2D>();
+            // get its origin
+            Vector2 origin = dancefloor.transform.position;
+            // get the halfsize of the npc to make sure that its origin is within the dance floor
+            float npc_size = npcPrefab.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+            // the radius where the npcs are allowed to spawn is adjusted with a safety factor and the npc size so that the npcs always spawn within
+            // the dancefloor even when they get placed near its edge
+            float radius = dancefloor.Radius-1.2f*npc_size;
+            // place NPCs randomly on dancefloor (contained by cirlce edge collider)
             for (int i = 1; i <= numNPCs; i++)
             {
-                NPCs.Add(Instantiate(npcPrefab.GetComponent<NPC>(),
-                                         new Vector3(2.5f, 0f, 0f),
+                // Sets the position to be somewhere inside a circle
+                // with radius of and center of dancefloor. Note that
+                // assigning a Vector2 to a Vector3 is fine - it will
+                // just set the X and Y values.          
+                Vector2 position = UnityEngine.Random.insideUnitCircle * radius;
+                position += origin;
+                              NPCs.Add(Instantiate(npcPrefab.GetComponent<NPC>(),
+                                        position,
                                         Quaternion.identity));
                 // give all npcs a unique id
                 NPCs[i - 1].myID = i - 1;
