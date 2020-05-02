d9321d8269099759683b18eb31da0bcef5b639e9
Johannes Holke <johannes.holke@dlr.de>
2020-05-02 17:47:28 +0200

    Insert a new level. This  is the base for the mask collect level

---
+++ YouVsVirus/Assets/Scripts/Components/EndLevelControllerLevelcollectmasks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+/// <summary>
+/// The end level controller for the collect masks.
+/// Currently we keep all settings from the base class,
+/// thus the level ends when the spread dies.
+/// </summary>
+public class EndLevelControllerLevelcollectmasks : EndLevelControllerBase
+{
+    /// <summary>
+    /// Triggers the end of the level.
+    /// </summary>
+    public override void EndLevel()
+    {
+        // Load end screen
+        UnityEngine.SceneManagement.SceneManager.LoadScene("EndScreenLevelcollectmasks");
+    }
+}
--- YouVsVirus/Assets/Scripts/UI/LevelcollectmasksContinueButton.cs
+++ YouVsVirus/Assets/Scripts/UI/LevelcollectmasksContinueButton.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 /// <summary>
 /// Load next end screen
 /// </summary>
-public class LevelsupermarketContinueButton : MonoBehaviour
+public class LevelcollectmasksContinueButton : MonoBehaviour
 {
     public void Continue()
     {
-        Debug.Log ("Continue from supermarket level.");
         // Continue Button has been pressed load next end screen
-        UnityEngine.SceneManagement.SceneManager.LoadScene("StartscreenLevelcollectmasks");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("EndScreenLevelsupermarket_1");
     }
 }
