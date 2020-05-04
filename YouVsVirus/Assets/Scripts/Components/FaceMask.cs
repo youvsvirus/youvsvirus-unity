186dff098cbd1a583d015d9cae2bd5f177f8b96e
Johannes Holke <johannes.holke@dlr.de>
2020-05-04 18:32:06 +0200

    The mask now finally collides with the player and then spawns at new position

---
+++ YouVsVirus/Assets/Scripts/Components/FaceMask.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Components
+{
+    /// The mask class does nothing, except being
+    /// a class that we can associate the mask sprit to.
+    public class FaceMask : MonoBehaviour
+    {
+        Camera MainCamera;
+
+        /// <summary>
+        /// the screen dimensions
+        /// </summary>
+        private Vector2 screenBounds;
+
+        void Start ()
+        {
+            Debug.Log ("I am a mask and i am starting.");
+            //This gets the Main Camera from the Scene
+            MainCamera = Camera.main;
+            // transform screen dimenensions into world space
+            screenBounds = MainCamera.GetComponent<CameraResolution>().GetMapExtents();
+        }
+
+        
+        void OnTriggerEnter2D (Collider2D other)
+        {
+            Debug.Log ("Collision detected.");
+            if (other.gameObject.tag == "Player") {
+                Debug.Log ("Player touched mask");
+                PlaceMaskRandomly();  
+            }
+        }
+
+        /// <summary>
+        /// Places one mask on the map randomly.
+        /// </summary>
+        private void PlaceMaskRandomly()
+        {
+            float bufferZoneX = screenBounds[0]/10;
+            float bufferZoneY = screenBounds[1]/10;
+            float distanceAway = 1;
+            Vector2 newPos = -screenBounds;
+            Vector2 oldPos = new Vector2(transform.position.x, transform.position.y);
+
+            do
+            {
+                newPos[0] = UnityEngine.Random.Range (-screenBounds[0] + bufferZoneX, screenBounds[0] - bufferZoneX);
+                newPos[1] = UnityEngine.Random.Range (-screenBounds[1] + bufferZoneY, screenBounds[1] - bufferZoneY);
+            }
+            while ((newPos - oldPos).magnitude < distanceAway);
+
+            Debug.Log ("Placing mask at " + newPos);
+            //  Place the mask
+            transform.position = newPos;
+        }
+    }
+}
---
+++ YouVsVirus/Assets/Scripts/Components/MaskControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+namespace Components
+{
+    public class MaskControl : MonoBehaviour
+    {
+        Camera MainCamera;
+
+        /// <summary>
+        /// mask prefab to be set in the editor.
+        /// </summary>
+        public GameObject maskPrefab;
+
+        public FaceMask mask;
+
+        private LevelSettings levelSettings;
+
+        /// <summary>
+        /// the screen dimensions
+        /// </summary>
+        private Vector2 screenBounds;
+
+        /// <summary>
+        /// The Sprites corresponding to the images for the different conditions
+        /// Images have to be set in the derived classes
+        /// </summary>     
+      //  protected Sprite MaskSprite=Resources.Load<Sprite>("Masks/mask");
+
+        // Awake is called the moment this component is created
+        void Awake()
+        {
+            levelSettings = LevelSettings.GetActiveLevelSettings();
+            //This gets the Main Camera from the Scene
+            MainCamera = Camera.main;
+            // transform screen dimenensions into world space
+            screenBounds = MainCamera.GetComponent<CameraResolution>().GetMapExtents();
+            // Place the mask on the map
+            PlaceMask();
+        }
+
+        /// <summary>
+        /// Places one mask on the map randomly.
+        /// </summary>
+        private void PlaceMask()
+        {
+            Vector2 origin = -screenBounds;
+            float bufferZoneX = screenBounds[0]/10;
+            float bufferZoneY = screenBounds[1]/10;
+
+            origin[0] = UnityEngine.Random.Range (-screenBounds[0] + bufferZoneX, screenBounds[0] - bufferZoneX);
+            origin[1] = UnityEngine.Random.Range (-screenBounds[1] + bufferZoneY, screenBounds[1] - bufferZoneY);
+
+            Debug.Log ("Placing mask at " + origin);
+            //  Place the mask
+            mask = Instantiate(maskPrefab.GetComponent<FaceMask>(), origin, 
+                               Quaternion.identity);
+        }
+    }
+}
