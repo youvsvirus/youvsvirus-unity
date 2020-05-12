23dd947f8d6120ed14ea6be188b53fa78052c84e
Johannes Holke <johannes.holke@dlr.de>
2020-05-12 17:23:57 +0200

    Spawnable space only applies to active colliders

--- YouVsVirus/Assets/Scripts/Components/nonSpawnableSpace.cs
+++ YouVsVirus/Assets/Scripts/Components/nonSpawnableSpace.cs
@@ -31,21 +31,24 @@ public class nonSpawnableSpace : MonoBehaviour
 
         colliders2D = GetComponents<CircleCollider2D>();
         foreach (CircleCollider2D collider in colliders2D) {
-            // Build a 2D vector with the colliders 2D center coordinates.
-            Vector2 colliderCenter = new Vector2(collider.offset[0] + transform.position[0],
-                                                 collider.offset[1] + transform.position[1]);
-            
-            Debug.Log ("SpawnableSpace: Circle with center " + colliderCenter + " and radius " + collider.radius);
-            Debug.Log ("SpawnableSpace: checking agains point " + coord2D);
+            if (collider.isActiveAndEnabled)
+            {
+                // Build a 2D vector with the colliders 2D center coordinates.
+                Vector2 colliderCenter = new Vector2(collider.offset[0] + transform.position[0],
+                                                    collider.offset[1] + transform.position[1]);
+                
+                Debug.Log ("SpawnableSpace: Circle with center " + colliderCenter + " and radius " + collider.radius);
+                Debug.Log ("SpawnableSpace: checking agains point " + coord2D);
 
-            if (Vector2.Distance (coord2D, colliderCenter) < collider.radius) {
-                // The coordinates lie within the collider
-                // This coordinate is non-spawnable
-                Debug.Log ("SpawnableSpace: Non spawnable");
-                return false;
+                if (Vector2.Distance (coord2D, colliderCenter) < collider.radius) {
+                    // The coordinates lie within the collider
+                    // This coordinate is non-spawnable
+                    Debug.Log ("SpawnableSpace: Non spawnable");
+                    return false;
+                }
             }
         }
-                Debug.Log ("SpawnableSpace: spawnable");
+        Debug.Log ("SpawnableSpace: spawnable");
         return true;
     }
 }
