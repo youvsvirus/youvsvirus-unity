e3e78ef9f690113375b45c9dfc54043c0859fa00
holke <johannes.holke@dlr.de>
2020-05-13 19:27:48 +0200

    Correct Spell error 2/3

--- YouVsVirus/Assets/Scripts/Components/LevelSettings.cs
+++ YouVsVirus/Assets/Scripts/Components/LevelSettings.cs
@@ -29,7 +29,7 @@ public class LevelSettings : MonoBehaviour
     /// <summary>
     /// Do we show the infection status of the smileys in this level or not.
     /// </summary>
-    public bool ShowInfectinoStatus = true;
+    public bool ShowInfectionStatus = true;
 
     /// <summary>
     /// the current active scene
@@ -87,4 +87,3 @@ public class LevelSettings : MonoBehaviour
         return ActiveEndLevelController;
     }
 }
-
