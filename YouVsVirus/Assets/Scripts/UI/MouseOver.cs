a5da3bab09fe37fb44a394624cda95adb0f9aa80
Klitz, Margrit <margrit.klitz@dlr.de>
2020-05-16 13:26:21 +0200

    Mouseover for endscreen
    Added mouse over descriptions for statistics on end screen.
    Changed  Killed by you / Infected by you text.
    Mouse over script can  now be used by dragging the text in the editor
    to the script or by the text being a child of the object that the text
    is attached to.

--- YouVsVirus/Assets/Scripts/UI/MouseOver.cs
+++ YouVsVirus/Assets/Scripts/UI/MouseOver.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 
+/// <summary>
+/// Shows text on mouse over
+/// Offers two possibilites: Either the text is a child of the game object
+/// you want to use for mouse over or you can drag it to the script in the editor
+/// *Note:* If the text is a child component it has to be set as active on start
+/// in the editor, otherwise we get a null reference.
+/// </summary>
 public class MouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject childText = null; //  or make public and drag
     void Start()
     {
-        TextMeshPro text = GetComponentInChildren<TextMeshPro>();
-        if (text != null)
+        // if no text object is assigned in the editor
+        if (childText == null)
         {
+            // try to find the text in my children
+            TextMeshProUGUI text = GetComponentInChildren<TextMeshProUGUI>();
             childText = text.gameObject;
+        }
+        // then set the text as inactive
+        if (childText != null)
+        {           
             childText.SetActive(false);
         }
     }
+    /// <summary>
+    /// On mouse over set the text as active
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerEnter(PointerEventData eventData)
     {
         childText.SetActive(true);
     }
+
+    /// <summary>
+    /// After mouse over deactivate text
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerExit(PointerEventData eventData)
     {
         childText.SetActive(false);
