7a12f975c876614ad0c6fabe1c21a44f6fffcf0c
Klitz, Margrit <margrit.klitz@dlr.de>
2020-05-17 18:14:22 +0200

    The end game controller base handles call of canvas
    The end game controller calls the succeed canvas when
    the player is home and healthy and it calls the other one
    when the player is exposed. All other end game controllers have
    to assign these game objects otherwise the other end game conditions
    are taken. For example, the sandbox level still works as usual
    since there are no fail/succ  canvas objects to assign. The
    end game controller also hanldes the cummulative sprite update.
    However, the children still use the CreateHuman-Array-NPC-List
    for that which should be done differently in the future.

--- YouVsVirus/Assets/Scripts/Components/EndLevelControllerBase.cs
+++ YouVsVirus/Assets/Scripts/Components/EndLevelControllerBase.cs
@@ -2,10 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using TMPro.EditorUtilities;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class EndLevelControllerBase : MonoBehaviour
 {
+    /// <summary>
+    /// fails screen in campaign
+    /// </summary>
+    public GameObject CanvasFail = null;
+    /// <summary>
+    /// success screen in campaign
+    /// </summary>
+    public GameObject CanvasSucc = null;
+
+    public GameObject NPCPrefab;
+
     protected float startTime = 0f;
 
     /// <summary>
@@ -36,6 +49,17 @@ public class EndLevelControllerBase : MonoBehaviour
         UnityEngine.Debug.Log("Set End Level Controller as active end level controller.");
         // Set this controller to be the current active end level controller
         LevelSettings.ActiveEndLevelController = this;
+    }
+
+    /// <summary>
+    /// if available deactivate both canvases before level starts
+    /// </summary>
+    public void Awake()
+    {
+        if(CanvasFail != null)
+            CanvasFail.SetActive(false);
+        if (CanvasSucc != null)
+            CanvasSucc.SetActive(false);
     }
 
     public void Start()
@@ -106,8 +130,44 @@ public class EndLevelControllerBase : MonoBehaviour
         activeInfections--;
     }
 
+    protected virtual void CummulativeSpriteUpdate()
+    {
+        // sometimes we do no need this, the other end level controlllers have to implement this if needed
+    }
+
+    protected void EndGamePlayerExposed()
+    {
+        // if the player is exposed we fail
+        if (CanvasFail != null && playerExposed)
+        {
+            // all NPCs show true infection statuts
+            if (LevelSettings.GetActiveLevelSettings().ShowInfectionStatus == false)
+            {
+                CummulativeSpriteUpdate();
+            }
+            CanvasFail.SetActive(true);
+        }
+    }
+
+    protected void EndGamePlayerAtHome()
+    {
+        // if the player is at home and well we win
+        if (CanvasSucc != null && playerHome && !playerExposed)
+        {
+            // all NPCs show true infection statuts
+            CummulativeSpriteUpdate();
+            CanvasSucc.SetActive(true);
+        }
+    }
+
     void Update()
     {
+        // if the player is exposed we fail
+        EndGamePlayerExposed();
+
+        // if the player is at home and well we win
+        EndGamePlayerAtHome();
+
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
