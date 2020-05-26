7b161c78576a3bd1c95de03ff870680824a38c28
Johannes Holke <johannes.holke@dlr.de>
2020-05-26 17:46:49 +0200

    Restore number of masks needed

--- YouVsVirus/Assets/Scripts/LevelCollectMasks/EndLevelControllerLevelcollectmasks.cs
+++ YouVsVirus/Assets/Scripts/LevelCollectMasks/EndLevelControllerLevelcollectmasks.cs
@@ -19,7 +19,7 @@ public class EndLevelControllerLevelcollectmasks : EndLevelControllerBase
     /// The number of masks that the player needs to collect
     /// to complete the level.
     /// </summary>
-    private const int numberOfMasksNeeded = 0;
+    private const int numberOfMasksNeeded = 8;
 
     /// <summary>
     /// Will get set to true if all masks were collected.
