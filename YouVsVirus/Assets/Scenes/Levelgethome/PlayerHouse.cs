c14a85ef03102086e8b7a2019b37d0ca26862910
Johannes Holke <johannes.holke@dlr.de>
2020-05-30 15:19:14 +0200

    A new version of the house.
    Coroutines are only startet whitin update
    and only if no other coroutines run.
    
    We have an additional trigger that is responsible
    for telling the house whether the player is close or not.

--- YouVsVirus/Assets/Scenes/Levelgethome/PlayerHouse.cs
+++ YouVsVirus/Assets/Scenes/Levelgethome/PlayerHouse.cs
@@ -36,8 +36,19 @@ public class PlayerHouse : MonoBehaviour
     /// </summary>
     private bool isPlayerInside = false;
 
-    private bool inHouseRuns = false;
-    private bool outHouseRuns = false;
+    private GameObject player;
+
+    private bool aCoroutineRuns = false;
+
+    private bool playerIsClose = false;
+
+    // Time variable used to perform update only every x seconds.
+    // We store here the last time that update was executed.
+    private float lastTime = 0;
+    
+    // How much time should pass before the next update is
+    // performed.
+    private float timeBetweenUpdates = 0.1f;
 
     private void Start()
     {
@@ -46,74 +57,34 @@ public class PlayerHouse : MonoBehaviour
         UnshowPlayer();
         // activate end level controller
         endlevel = LevelSettings.GetActiveEndLevelController();
+        // Get player (this may return null if the player is inactive)
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // Set this point in time as the last execution time of update.
+        // This means, update is first performed at time Time.time + timeBetweenUpdates
+        lastTime = Time.time;
     }
 
-
-    /// <summary>
-    /// Check if someone knocks on our door
-    /// maybe we end the game here
-    /// or we allow the player to hide
-    /// with the space-key the player gets in and out of the house
-    /// </summary>
-    /// <param name="other"> the other collider which can only be the player </param>
-    void OnTriggerEnter2D(Collider2D other)
-    {     
-        // normally only the player will be able to get here
-        // so this is just a safety measure
-        // the collider attached to the infection trigger is hit first
-        // hence we have to get its "parent" the player
-        if (other.gameObject.GetComponentInParent<HumanBase>() != null)
-        {
-            if (other.gameObject.GetComponentInParent<HumanBase>().tag == "Player")
-            {
-                Debug.Log ("Trigger Enter");
-                // the player is not inside
-                if (!isPlayerInside)
-                {                   
-                    if (LevelSettings.GetActiveSceneName() == "YouVsVirus_Levelgethome" || LevelSettings.GetActiveSceneName() == "YouVsVirus_Levelcollectmasks")
-                    {
-                        Debug.Log ("Trigger Enter Starting coroutine SetPlayerInHouse");
-                        //activate player in house, deactivate player, end game
-                        StartCoroutine (SetPlayerInHouse(other.gameObject.GetComponentInParent<HumanBase>().gameObject));
-                    }
-                    // in this level we have to get home and get toilet paper
-                    else if (LevelSettings.GetActiveSceneName() == "YouVsVirus_Levelsupermarket" && other.gameObject.GetComponentInParent<Player>().hasToiletpaper)
-                    {
-                        //activate player in house, deactivate player, end game
-                        StartCoroutine (SetPlayerInHouse(other.gameObject.GetComponentInParent<HumanBase>().gameObject));
-                    }
-                }                                              
-            }
-        }
-    }
-
-    /// <summary>
-    /// On exiting the player house collider, we have to stop all coroutines
-    /// Otherwise the user could simply press "space" to get the player in the house
-    /// without being near it. We simply stop all coroutines here.
-    /// </summary>
-    /// <param name="other">the player</param>
-    void OnTriggerExit2D(Collider2D other)
+    // Tell the house whether the player is close or not
+    public void NotifyPlayerIsClose (bool IsClose)
     {
-        // the player is not in the house, i.e. he has left
-        if (!isPlayerInside)
-        {
-            // normally only the player will be able to get here
-            // so this is just a safety measure
-            // the collider attached to the infection trigger is hit first
-            // hence we have to get its "parent" the player
-            if (other.gameObject.GetComponentInParent<HumanBase>() != null)
-            {
-                if (other.gameObject.GetComponentInParent<HumanBase>().tag == "Player")
-                {
-                    // A MonoBehaviour can execute zero or more coroutines. 
-                    // StopAllCoroutines is used to stop both coroutines used here.
-                    // No arguments are needed because all coroutines on a script are stopped.
-                    StopAllCoroutines();                    
-                }
-            }
-        }
+        playerIsClose = IsClose;
     }
+
+    
+    // Tell the house whether the player is inside or not
+    public void NotifyPlayerInside (bool IsInide)
+    {
+        isPlayerInside = IsInide;
+    }
+
+    /// <summary> Query whether the player is currently inside </summary>
+    /// <return> isPlayerInside </return>
+    public bool isInside ()
+    {
+        return isPlayerInside;
+    }
+ 
 
     public void ShowPlayer()
     {
@@ -126,45 +97,38 @@ public class PlayerHouse : MonoBehaviour
     }
 
     /// <summary>
+    /// Set the internal player object.
+    /// This should be called from the object that
+    /// instantiates the player. 
+    /// Reaon: Sometimes the player object
+    /// is not active but the house needs to avtivate it.
+    /// But we cannot find unactive objects with GameObject.find
+    /// </summary>
+    public void setPlayer (GameObject setPlayer)
+    {
+        player = setPlayer;
+    }
+
+    /// <summary>
     /// waits till space-key is pressed to set player out of house
     /// </summary>
     /// <param name="p"> the player game object</param>
     /// <returns></returns>
-    private IEnumerator SetPlayerOutOfHouse(GameObject p)
+    private IEnumerator SetPlayerOutOfHouse()
     {
-        //// Check if other coroutine is running and terminate it.
-        //// This is to prevent SetPlayerInHouse and SetPlayerOutOfHouse
-        //// from running simultaeously.
         Debug.Log ("Starting coroutine OutHouse");
-        //if (outHouseRuns || inHouseRuns) {
-        //    // This coroutine already runs in another instance, we terminate.
-        //    // Or the SetPlayerInHouse coroutine still runs, we terminate.
-        //    yield break;
-        //}
-        //outHouseRuns = true;
-        // do nothing until key is pressed
-        do
-        {
-            yield return null;
-        } while (!Input.GetKeyDown(KeyCode.Space));
+        aCoroutineRuns = true;
 
         // get player out of house again
         UnshowPlayer();
-        p.SetActive(true);
+        player.SetActive(true);
         isPlayerInside = false;
         // Notify the endlevel controller that we left the home
         endlevel.NotifyPlayerLeftHome();
-        outHouseRuns = false;
-
        
+        aCoroutineRuns = false;
 
-        //// Enable the player to enter again, if desired
-        //if (EnterAndExitAtWill)
-        //{
-        //    // If still running, stop coroutine Set in house to avoid infinite loop
-        //    StopCoroutine (SetPlayerInHouse(p));
-        //    StartCoroutine(SetPlayerInHouse(p));
-        //}
+        yield return 0;
     }
 
     /// <summary>
@@ -174,31 +138,15 @@ public class PlayerHouse : MonoBehaviour
     /// All with a little bit of time delay to make it look good
     /// Calls SetPlayerOutOfHouse
     /// </summary>
-    /// <param name="p"> the player game object</param>
     /// <returns></returns>
-    private IEnumerator SetPlayerInHouse(GameObject p)
+    private IEnumerator SetPlayerInHouse()
     {
         Debug.Log ("Starting coroutine InHouse");
-        //if (inHouseRuns || outHouseRuns) {
-        //    // This coroutine already runs in another instance, we terminate.
-        //    // Or the SetPlayerOutOfHouse coroutine still runs, we terminate.
-        //    yield break;
-        //}
-        //inHouseRuns = true;
+        aCoroutineRuns = true;
 
-        // Give the sysyem some time to wait and stop the other coroutines
-        // before asking for space key input
-        //yield return new WaitForSeconds(0.05f);
-        if (EnterAndExitAtWill)
-        {
-            do
-            {
-                yield return null;
-            } while (!Input.GetKeyDown(KeyCode.Space));
-        }
         // takes a little while for the player to get inside and disappear
         yield return new WaitForSeconds(0.3f);
-        p.SetActive(false);
+        player.SetActive(false);
         // takes some more time until the player looks outside the window
         yield return new WaitForSeconds(1f);
         ShowPlayer();
@@ -213,9 +161,6 @@ public class PlayerHouse : MonoBehaviour
         //FIXME: has to be implemented in endlevel controller base
         endlevel.NotifyPlayerAtHome();
 
-        inHouseRuns = false;
-
-        yield return StartCoroutine(SetPlayerOutOfHouse(p));
         // maybe the player wants to get out again
         //if (EnterAndExitAtWill)
         //{
@@ -224,5 +169,45 @@ public class PlayerHouse : MonoBehaviour
         //    StartCoroutine(SetPlayerOutOfHouse(p));
         //}
         Debug.Log("End of Coroutine");
+        aCoroutineRuns = false;
+    }
+
+    void Update ()
+    {
+        // TODO: Only do this every 0.1 secs or so to increase performance
+        if (Time.time - lastTime >= timeBetweenUpdates)
+        {
+            Debug.Log("House: inside " + isPlayerInside + " close " + playerIsClose + " crruns " + aCoroutineRuns);
+            lastTime = Time.time;
+        }
+        if (player == null) {
+            // Do nothing is we do not have a reference of the player.
+            // This reference can be set withe the setPlayer function,
+            // or the player object was found in the Start routine.
+            return;
+        }
+        if (playerIsClose) {
+            // Only do smth if the player is close
+            if (isPlayerInside) {
+            // We are inside, check space to get outside
+                if (Input.GetKeyDown(KeyCode.Space) && !aCoroutineRuns) { 
+                    aCoroutineRuns = true;
+                    StartCoroutine(SetPlayerOutOfHouse());
+                }
+            }
+            else if (endlevel.isPlayerAllowedHome()) {
+                // We are not inside, but are allowed to enter
+                if (!EnterAndExitAtWill || Input.GetKeyDown(KeyCode.Space) && !aCoroutineRuns) {
+                    if (!aCoroutineRuns) {
+                        // Either we are in enter-with-space mode and the space key was pressed,
+                        // or we are in just-enter mode.
+                        // we enter the house
+                        aCoroutineRuns = true;
+                        StartCoroutine(SetPlayerInHouse());
+                    }
+                }
+            }
+        }
     }
 }
+
---
+++ YouVsVirus/Assets/Scripts/Components/playerIsCloseToHouse.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is part of the player house.
+// It belongs to the trigger that is responsible
+// for telling the house whether or not the player is
+// in proximity and may therefore enter.
+// We put it in a seperate game object together
+// with the trigger in order to ensure that
+// the OnTriggerEnter/Exit functions are only
+// called by the specified trigger and not the
+// other triggers that are associated to the house.
+namespace Components
+{
+public class playerIsCloseToHouse : MonoBehaviour
+{
+    public GameObject playerHouse;
+
+    /// <summary>
+    /// Check if someone knocks on our door
+    /// maybe we end the game here
+    /// or we allow the player to hide
+    /// with the space-key the player gets in and out of the house
+    /// </summary>
+    /// <param name="other"> the other collider which can only be the player </param>
+    void OnTriggerEnter2D(Collider2D other)
+    {     
+        if (other.gameObject.GetComponentInParent<HumanBase>() != null) 
+        {
+            if (other.gameObject.GetComponentInParent<HumanBase>().tag == "Player")     
+            {
+                 Debug.Log("Player enter");
+                 // Tell the house that the player is now close to it
+                 playerHouse.GetComponent<PlayerHouse>().NotifyPlayerIsClose (true);
+            }
+        }
+    }
+
+    /// <summary>
+    /// On exiting the player house collider, we have to stop all coroutines
+    /// Otherwise the user could simply press "space" to get the player in the house
+    /// without being near it. We simply stop all coroutines here.
+    /// </summary>
+    /// <param name="other">the player</param>
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // the player is not in the house, i.e. he has left
+        if (!playerHouse.GetComponent<PlayerHouse>().isInside())
+        {   
+            if (other.gameObject.GetComponentInParent<HumanBase>() != null) 
+            {
+                if (other.gameObject.GetComponentInParent<HumanBase>().tag == "Player")     
+                {
+                    Debug.Log("Player exit");
+                    // Tell the house that the player is now away from it
+                    playerHouse.GetComponent<PlayerHouse>().NotifyPlayerIsClose (false);
+                }
+            }
+        }
+    }
+}
+}
