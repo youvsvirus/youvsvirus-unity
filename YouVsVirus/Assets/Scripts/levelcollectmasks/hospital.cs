3944c820fde127ce8082635e52441cdc34696903
Johannes Holke <johannes.holke@dlr.de>
2020-06-04 13:33:42 +0200

    hospital -> Hospital

--- YouVsVirus/Assets/Scripts/levelcollectmasks/hospital.cs
+++ YouVsVirus/Assets/Scripts/levelcollectmasks/hospital.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Components
 {
-    public class hospital : MonoBehaviour
+    public class Hospital : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
